0b0d055306cd1e41ddbaff28504d9908246a6a15
Glenn Goossens <glenngoossens07@gmail.com>
2017-05-28 21:23:24 +0200

    adding bluetoothscanner

--- app/src/main/java/com/glenngoossens/yan/DeviceControlActivity.java
+++ app/src/main/java/com/glenngoossens/yan/DeviceControlActivity.java
@@ -8,6 +8,8 @@ public class DeviceControlActivity extends Activity {
 
     private final static String TAG = DeviceControlActivity.class.getSimpleName();
 
+    public static final String EXTRAS_DEVICE_NAME = "DEVICE_NAME";
+    public static final String EXTRAS_DEVICE_ADDRESS = "DEVICE_ADDRESS";
 
 
     @Override
