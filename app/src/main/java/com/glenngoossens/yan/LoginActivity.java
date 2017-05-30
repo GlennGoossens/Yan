f35008e09f0eb7f5edee2337b26ed128d0ffeb53
Glenn Goossens <glenngoossens07@gmail.com>
2017-05-30 21:21:41 +0200

    adding database to login

--- app/src/main/java/com/glenngoossens/yan/LoginActivity.java
+++ app/src/main/java/com/glenngoossens/yan/LoginActivity.java
@@ -16,9 +16,15 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import org.w3c.dom.Text;
 
+import java.util.ArrayList;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -28,10 +34,14 @@ public class LoginActivity extends Activity implements View.OnClickListener {
     private FirebaseAuth mAuth;
     private FirebaseAuth.AuthStateListener mAuthListener;
 
-    private EditText emailField,passwordField;
+    private EditText emailField, passwordField;
     private TextView changeSignUpModeTextView;
     private boolean signUpActive;
     private Button signUpButton;
+
+    private FirebaseDatabase database = FirebaseDatabase.getInstance();
+    private DatabaseReference myRef = database.getReference("database");
+    private ArrayList<String> emails = new ArrayList<>();
 
 
     @Override
@@ -43,7 +53,13 @@ public class LoginActivity extends Activity implements View.OnClickListener {
         passwordField = (EditText) findViewById(R.id.passwordEditText);
         changeSignUpModeTextView = (TextView) findViewById(R.id.logInTextView);
         signUpActive = true;
-        signUpButton =(Button) findViewById(R.id.signInButton);
+        signUpButton = (Button) findViewById(R.id.signInButton);
+        signUpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                signUpOrLogin(v);
+            }
+        });
 
         changeSignUpModeTextView.setOnClickListener(this);
 
@@ -52,9 +68,9 @@ public class LoginActivity extends Activity implements View.OnClickListener {
             @Override
             public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
                 FirebaseUser user = firebaseAuth.getCurrentUser();
-                if(user !=null){
+                if (user != null) {
                     Log.d(TAG, "onAuthStateChanged: signed in");
-                }else{
+                } else {
                     Log.d(TAG, "onAuthStateChanged: signed out");
                 }
             }
@@ -73,34 +89,58 @@ public class LoginActivity extends Activity implements View.OnClickListener {
         mAuth.addAuthStateListener(mAuthListener);
     }
 
-    public void createAccount(String email,String password){
-        mAuth.createUserWithEmailAndPassword(email,password)
-                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        Log.d(TAG, "onComplete: create " + task.isSuccessful());
+    public void createAccount(final String email, String password) {
+        myRef.child("users").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                for (DataSnapshot dataSnapshot1 : dataSnapshot.getChildren()) {
+                   if(email.equals(dataSnapshot1.getValue().toString())){
+                       Toast.makeText(getApplicationContext(), "Email already exists", Toast.LENGTH_SHORT).show();
+                       signUpActive = false;
+                       changeSignUpModeTextView.setText("Sign Up");
+                       signUpButton.setText("Log In");
+                       return;
+                   }
+                }
+                Log.i(TAG, "onDataChange: " + emails.toString());
+            }
 
-                        if(!task.isSuccessful()){
-                            Toast.makeText(LoginActivity.this,"creation failed",Toast.LENGTH_SHORT).show();
-                        }else{
-                            Toast.makeText(LoginActivity.this,"creation of user is successful",Toast.LENGTH_SHORT).show();
-                            FirebaseUser userFirebase = mAuth.getCurrentUser();
-                            Log.i(TAG, "CurrentUser creation: " + userFirebase.getEmail());
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                Toast.makeText(getApplicationContext(), "Failed to read data", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+            mAuth.createUserWithEmailAndPassword(email, password)
+                    .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            Log.d(TAG, "onComplete: create " + task.isSuccessful());
+
+                            if (!task.isSuccessful()) {
+                                Toast.makeText(LoginActivity.this, "creation failed", Toast.LENGTH_SHORT).show();
+                            } else {
+                                Toast.makeText(LoginActivity.this, "creation of user is successful", Toast.LENGTH_SHORT).show();
+                                FirebaseUser userFirebase = mAuth.getCurrentUser();
+                                myRef.child("users").push().setValue(email);
+                                Log.i(TAG, "CurrentUser creation: " + userFirebase.getEmail());
+                            }
                         }
-                    }
-                });
-    }
+                    });
+        }
 
-    public void signIn(String email, String password){
+
+    public void signIn(String email, String password) {
         mAuth.signInWithEmailAndPassword(email, password)
                 .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         Log.d(TAG, "onComplete: login " + task.isSuccessful());
-                        if(!task.isSuccessful()){
-                            Toast.makeText(LoginActivity.this,"login failed",Toast.LENGTH_SHORT).show();
-                        }else{
-                            Toast.makeText(LoginActivity.this,"user is logged in",Toast.LENGTH_SHORT).show();
+                        if (!task.isSuccessful()) {
+                            Toast.makeText(LoginActivity.this, "login failed", Toast.LENGTH_SHORT).show();
+                        } else {
+                            Toast.makeText(LoginActivity.this, "user is logged in", Toast.LENGTH_SHORT).show();
                         }
                     }
                 });
@@ -108,11 +148,12 @@ public class LoginActivity extends Activity implements View.OnClickListener {
 
 
     public void signUpOrLogin(View view) {
+        Log.i(TAG, "signUpOrLogin: entering");
         String email = String.valueOf(emailField.getText());
         String pass = String.valueOf(passwordField.getText());
-        if(email.equals("") || pass.equals("")){
-            Toast.makeText(LoginActivity.this,"Please fill in the fields",Toast.LENGTH_SHORT).show();
-        }else {
+        if (email.equals("") || pass.equals("")) {
+            Toast.makeText(LoginActivity.this, "Please fill in the fields", Toast.LENGTH_SHORT).show();
+        } else {
             Pattern p = Pattern.compile(".+@.+\\.[a-z]+");
             Matcher m = p.matcher(email);
             boolean matchFound = m.matches();
@@ -120,28 +161,29 @@ public class LoginActivity extends Activity implements View.OnClickListener {
                 Toast.makeText(LoginActivity.this, "Please enter a valid email", Toast.LENGTH_SHORT).show();
                 if (pass.length() < 6) {
                     Toast.makeText(LoginActivity.this, "Password must have more than 6 digits", Toast.LENGTH_SHORT).show();
-                } else {
-                    if (signUpActive) {
-                        createAccount(email, pass);
+                }
+            } else {
+                if (signUpActive) {
+                    createAccount(email, pass);
 
-                    } else {
-                        signIn(email, pass);
-                    }
+                } else {
+                    signIn(email, pass);
                 }
             }
         }
     }
 
+
     @Override
     public void onClick(View v) {
-        if(v.getId() == R.id.logInTextView){
+        if (v.getId() == R.id.logInTextView) {
             Log.i(TAG, "onClick: loginText");
-            if(signUpActive){
+            if (signUpActive) {
                 signUpActive = false;
                 changeSignUpModeTextView.setText("Sign Up");
                 signUpButton.setText("Log In");
 
-            }else{
+            } else {
                 signUpActive = true;
                 changeSignUpModeTextView.setText("Log In");
                 signUpButton.setText("Sign Up");
